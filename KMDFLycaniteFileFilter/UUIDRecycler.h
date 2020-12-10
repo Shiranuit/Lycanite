e92849d873042336e4d2afc38b216398f9d1c6aa
Shiranuit <shiranui007@gmail.com>
2020-12-10 21:24:22 +0100

    add type safetype on calloc

--- KMDFLycaniteFileFilter/UUIDRecycler.h
+++ KMDFLycaniteFileFilter/UUIDRecycler.h
@@ -28,11 +28,13 @@ UUIDRecycler* UUIDRecycler_create(UINT64 initialCapacity) {
 }
 
 void UUIDRecycler_recycleUUID(UUIDRecycler* recycler, UUID uuid) {
-	if (recycler != NULL) {
+	if (recycler != NULL && recycler->uuids) {
 		if (recycler->size + 1 > recycler->capacity) {
 			recycler->capacity += (recycler->capacity >> 1) | 1;
 			UUID* newUUIDs = (UUID*)calloc(recycler->capacity, sizeof(UUID));
-			RtlCopyMemory(newUUIDs, recycler->uuids, recycler->size * sizeof(UUID));
+			if (newUUIDs != NULL) {
+				RtlCopyMemory(newUUIDs, recycler->uuids, recycler->size * sizeof(UUID));
+			}
 			free(recycler->uuids);
 			recycler->uuids = newUUIDs;
 		}
@@ -42,7 +44,7 @@ void UUIDRecycler_recycleUUID(UUIDRecycler* recycler, UUID uuid) {
 
 
 UUID UUIDRecycler_getUUID(UUIDRecycler* recycler) {
-	if (recycler != NULL) {
+	if (recycler != NULL && recycler->uuids) {
 		if (recycler->size == 0) {
 			return recycler->lastID++;
 		}
